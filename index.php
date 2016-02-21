9632810395a1e1615bf955bbd67e4609ec57a518
sdantuoni <nico.dantuoni@hotmail.com>
2016-02-21 01:07:48 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+	<!-- Style CSS -->
+	<link href="style.css" rel="stylesheet">
+	<link href="css/colors.css" rel="stylesheet">
+
+	<!-- Royal Slider -->
+    <link href="royalslider/royalslider.css" rel="stylesheet">
+    <link href="royalslider/skins/default-inverted/rs-default-inverted.css" rel="stylesheet">
+
+	<!-- Layer Slider -->
+	<link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
+    
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="assets/js/html5shiv.js"></script>
+	  <script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="assets/ico/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="assets/ico/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/ico/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/ico/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/ico/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="assets/ico/apple-touch-icon-144x144.png">
+
+	<!-- Switcher Only -->
+	<link rel="stylesheet" id="switcher-css" type="text/css" href="switcher/css/switcher.css" media="all" />
+    <!-- END Switcher Styles -->
+
+	<!-- Demo Examples -->
+	<link rel="alternate stylesheet" type="text/css" href="switcher/css/blue.css" title="blue" media="all" />
+	<link rel="alternate stylesheet" type="text/css" href="switcher/css/yellow.css" title="yellow" media="all" />
+	<link rel="alternate stylesheet" type="text/css" href="switcher/css/red.css" title="red" media="all" />
+	<link rel="alternate stylesheet" type="text/css" href="switcher/css/green.css" title="green" media="all" />
+	<link rel="alternate stylesheet" type="text/css" href="switcher/css/dark.css" title="dark" media="all" />
+	<!-- END Demo Examples -->
+
+</head>
+<body>
+	<div id="wrapper">
+	<div class="topbar">
+    	<div class="container">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            </div><!-- end col-lg-6 -->
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            </div><!-- end col-lg-6 -->
+        </div><!-- end container -->
+    </div><!-- end topbar -->
+
+	<header class="header-wrapper">
+    	<div class="container">
+        	<div class="site-header clearfix">
+                <div class="col-lg-3 col-md-3 col-sm-12 title-area pull-left">
+                    <div class="site-title" id="title">
+                        <a href="index.html" title="">
+                            <img src="images/logo.png" alt="">	
+                        </a>
+                    </div><!-- site title -->
+                </div><!-- title area -->
+                <div class="col-lg-9 col-md-9 col-sm-12">
+                   <div id="nav">
+                        <div class="container clearfix">
+                            <ul id="jetmenu" class="jetmenu blue">
+                                <li class="active"><a href="#"><i class="fa fa-home"></i> Inicio</a>
+                                </li>
+                   <li><a href="#"><i class="fa fa-road"></i> Vehiculos</a></li>
+                                 <li><a href="#"><i class="fa fa-usd"></i> Propiedades</a></li>
+                                 <li><a href="#"><i class="fa fa-shopping-cart"></i> Tienda</a></li>
+                                <li><a href="#"><i class="fa fa-user"></i> Personaje</a></li>
+                                <li><a href="#"><i class="fa fa-envelope-o"></i> Contacto</a></li>
+                            </ul>
+                        </div>
+                    </div><!-- nav -->   
+              	</div><!-- end col 9 --> 
+			</div><!-- end site header -->
+    	</div><!-- end container -->         
+	</header><!-- end header-wrapper -->
+	<section class="section-whitebg">
+    	<div class="container">
+			<div class="section-title text-center">
+				<h2 class="bigtitle">New Clan Roleplay</h2>
+				<h4 class="lead">Bienvenido A Nuestra Nueva APP</h4>
+                <h1>Hola Mundo</h1>
+				<hr>
+			</div><!-- end section-title -->
+            
+		
+  
+	
+
+        </div>
+
+    
+	
+
+	<section class="section-whitebg dm-shadow">
+    	<div class="container">
+			<div class="section-title text-center">
+				<h2 class="bigtitle"> ¿Que esperas para ingresar a tu cuenta? </h2>
+				<h4 class="lead">Ingresa ahora para poder acceder a todas las características</h4>
+				<hr>
+			</div><!-- end section-title -->
+		
+            
+
+			<div class="text-center clearfix">
+				<a class="btn btn-primary btn-lg" href="login.php" title=""><i class="fa fa-pencil-square-o"></i> INGRESAR </a>
+			</div><!-- end text-center -->   
+                
+        </div><!-- end container -->
+    </section><!-- end section-whitebg -->
+    
+	<section class="section-colorizedbg">
+    	<div class="container">
+            <div class="message text-center">
+            <h2 class="bigtitle">¿No tienes una cuenta?</h2>
+            <p class="lead">Crea una cuenta y descubre el maravilloso mundo de New Clan Roleplay</p>
+            <a class="btn btn-primary btn-lg" href="login.php"><i class="fa fa-user"></i> Crear Cuenta</a>
+            </div>
+        </div><!-- end container -->
+    </section><!-- end section-transparentbg -->
+
+	<br>
+	
+
+	<section class="section-copyright dm-shadow text-center">
+		<div class="container">
+            <div class="back-to-top clearfix">
+                <span><span class="dmtop"><i class="fa fa-arrow-up"></i></span></span>
+            </div>
+			<p>Copyright © 2016 New Clan Roleplay</p>
+		</div><!-- end container -->
+    </section><!-- end section-copyright -->
+      
+</div><!-- wrapper -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jetmenu.js"></script>
+    <script src="js/jquery.fitvids.js"></script>    
+	<script src="js/jquery.unveilEffects.js"></script>	
+	<script src="js/retina-1.1.0.js"></script>
+	<script src="js/jquery.jigowatt.js"></script>
+	<script src="js/jquery.simple-text-rotator.js"></script>
+    <script src="js/application.js"></script>
+    
+	<!-- LayerSlider script files -->
+	<script src="layerslider/js/greensock.js" type="text/javascript"></script>
+	<script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+	<script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+    <script type="text/javascript">
+		jQuery("#layerslider").layerSlider({
+			pauseOnHover: false,
+			autoPlayVideos: false,
+			responsive: false,
+			responsiveUnder: 1280,
+			layersContainer: 1280,
+			skin: 'v5',
+			skinsPath: 'layerslider/skins/'
+		});
+	</script>
+
+	<!-- Owl Carousel script files -->
+	<script src="js/owl.carousel.js"></script>
+	<script>
+    $(document).ready(function() {
+		$("#owl-portfolio").owlCarousel({
+			items : 4,
+			lazyLoad : true,
+			pagination : false,
+			navigation : true
+		});
+		$("#owl-blog").owlCarousel({
+			items :3,
+			lazyLoad : true,
+			pagination : false,
+			navigation : true
+		});
+    });
+	</script>
+
+	<!-- Royal Slider script files -->
+	<script src="royalslider/jquery.royalslider.min.js"></script>
+	<script>
+    jQuery(document).ready(function($) {
+      var rsi = $('#slider-in-laptop').royalSlider({
+        autoHeight: false,
+        arrowsNav: false,
+        fadeinLoadedSlide: false,
+        controlNavigationSpacing: 0,
+        controlNavigation: 'bullets',
+        imageScaleMode: 'fill',
+        imageAlignCenter: true,
+        loop: false,
+        loopRewind: false,
+        numImagesToPreload: 6,
+        keyboardNavEnabled: true,
+        autoScaleSlider: true,  
+        autoScaleSliderWidth: 486,     
+        autoScaleSliderHeight: 315,
+    
+        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+        imgWidth: 792,
+        imgHeight: 479
+      }).data('royalSlider');
+  
+    });
+    </script>
+
+	<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script src="js/jquery.gmap.min.js"></script>
+	<script>
+	// MAP
+   jQuery(document).ready(function($) {
+        $("#responsive_map").gMap({
+             maptype: google.maps.MapTypeId.ROADMAP, 
+             zoom: 14, 
+             markers: [{
+                 latitude: -37.801578, 
+                 longitude: 145.060508, 
+                 html: "<img src='images/logo.png' width='69' height='27'><br/>Your address or description here.<br><strong>Drag the marker anywhere!</strong>", 
+                 popup: true, 
+                 flat: true, 
+                 icon: { 
+                     image: "images/tunnel.png", 
+                     iconsize: [32, 37], 
+                     iconanchor: [15, 30], 
+                     shadow: "images/icon-shadow.png", 
+                     shadowsize: [32, 37], 
+                     shadowanchor: null}
+                    } 
+                ], 
+             panControl: true, 
+             zoomControl: true, 
+             mapTypeControl: true, 
+             scaleControl: true, 
+             streetViewControl: true, 
+             scrollwheel: false, 
+             styles: [ { "stylers": [ { "hue": "#007ACC" }, { "gamma": 0 } ] } ], 
+             onComplete: function() {
+                 // Resize and re-center the map on window resize event
+                 var gmap = $("#responsive_map").data('gmap').gmap;
+                 window.onresize = function(){
+                     google.maps.event.trigger(gmap, 'resize');
+                     $("#responsive_map").gMap('fixAfterResize');
+                 };
+            }
+        });
+    });
+	</script>
+    
+   <!-- ADD Switcher -->
+            <div class="demo_changer text-center">
+                <div class="demo-icon"></div>
+                    <div class="form_holder">
+                        <div class="line"></div>
+                            <div class="col-lg-12">
+                            	<p>COLORS SCHEME</p>
+                                <div class="predefined_styles">
+                                    <a href="#" rel="blue" class="styleswitch"><img src="switcher/images/blue.jpg" alt=""></a>	
+                                    <a href="#" rel="red" class="styleswitch"><img src="switcher/images/red.jpg" alt=""></a>	
+                                    <a href="#" rel="green" class="styleswitch"><img src="switcher/images/green.jpg" alt=""></a>	
+                                    <a href="#" rel="dark" class="styleswitch"><img src="switcher/images/dark.jpg" alt=""></a>	
+                                    <a href="#" rel="yellow" class="styleswitch"><img src="switcher/images/yellow.jpg" alt=""></a>	
+                                </div>
+                            </div>
+						<div class="line"></div>
+							<a rel="stylesheet" class="btn btn-primary styleswitch" href="indexc392.html?default=true">RESET STYLE</a>
+						<div class="line"></div>
+                 </div><!-- END Form Holder -->
+            </div><!-- END Switcher -->
+    
+	<!-- Demo Switcher JS -->
+	<script type="text/javascript" src="switcher/js/fswit.js"></script> 
+    
+</body>
+</html>
